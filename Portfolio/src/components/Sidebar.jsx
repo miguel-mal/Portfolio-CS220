0fb9ca1776bd47c869f0f974c498f0f663a8cd18
miguel-mal <gmiguelmaldonado@gmail.com>
2025-06-20 21:01:35 -0600

    components revised

--- Portfolio/src/components/Sidebar.jsx
+++ Portfolio/src/components/Sidebar.jsx
@@ -8,22 +8,21 @@ function Sidebar() {
           className="w-48 h-48 rounded-full object-cover mb-4 z-50"
         />
         <div className="text-center space-y-2">
-          <p className="font-bold text-lg">Miguel Maldonado Gutierrez</p>
+          <p className="font-bold text-lg">Miguel M.G.</p>
           <p className="text-sm text-gray-600">miguel@ensign.com</p>
           <p className="text-sm text-gray-600">(123) 456-7890</p>
         </div>
       </div>
       <div>
-        {/* link to github and social media*/}
         <div className="fixed bottom-0 right-0 mb-4 mr-4 flex space-x-4">
-          <a href="github.com/miguelmaldonado" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.github.com/miguel-mal" target="_blank" rel="noopener noreferrer">
             <img 
               src="/git.svg" 
               alt="GitHub" 
               className="w-8 h-8"
             />
           </a>
-          <a href="https://www.linkedin.com/in/miguelmaldonado" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/miguel-mal/" target="_blank" rel="noopener noreferrer">
             <img 
               src="/linkedin.svg" 
               alt="LinkedIn" 
